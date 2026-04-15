83923732c6457c7e2b3000ba1c1cd0604691c64e
Nathaniel Trueba <ntrueba@ucsd.edu>
2026-04-15 12:55:07 -0700

    finished 3.2

--- global.js
+++ global.js
@@ -21,22 +21,22 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
+    let url = p.url;
+    let title = p.title;
 
-  url = !url.startsWith('http') ? BASE_PATH + url : url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
 
-  nav.insertAdjacentHTML(
-    'beforeend',
-    `<a href="${url}">${title}</a>`
-  );
-}
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
 
+    if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+    }
 
-let navLinks = $$("nav a");
+    if (a.host !== location.host) {
+    a.target = '_blank';
+    }
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-
-currentLink?.classList.add('current');
+    nav.append(a);
+    }
