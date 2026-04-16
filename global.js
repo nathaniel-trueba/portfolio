e5aac7c74665b9bd7823d04b6b3cf61ee60a8b75
Nathaniel Trueba <ntrueba@ucsd.edu>
2026-04-16 13:45:03 -0700

    finished lab 3

--- global.js
+++ global.js
@@ -17,6 +17,20 @@ let pages = [
   { url: 'https://github.com/nathaniel-trueba', title: 'GitHub' }
 ];
 
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `
+);
+
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
@@ -40,3 +54,40 @@ for (let p of pages) {
 
     nav.append(a);
     }
+
+
+let select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+  let value = event.target.value;
+
+  setColorScheme(value);
+  localStorage.colorScheme = value;
+});
+
+if ('colorScheme' in localStorage) {
+  let saved = localStorage.colorScheme;
+
+  setColorScheme(saved);
+  select.value = saved;
+}
+
+let form = document.querySelector('form');
+
+form?.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+  let params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${name}=${encodeURIComponent(value)}`);
+  }
+
+  let url = form.action + '?' + params.join('&');
+  location.href = url;
+});
