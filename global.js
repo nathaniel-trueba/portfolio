ce52d310ef76ecfd2c9229f1a8f715ec34029b99
Nathaniel Trueba <ntrueba@ucsd.edu>
2026-04-24 10:36:29 -0700

    through step 3

--- global.js
+++ global.js
@@ -120,3 +120,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   }
 }
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
